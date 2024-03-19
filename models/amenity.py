f56b067ad192476cfdb1c4b968ba02ef6edf88e9
taofeekajibade <cleargoal.ng@gmail.com>
2024-03-19 11:51:05 +0100

    added models and test suites

---
+++ models/amenity.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" State Module for HBNB project """
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+from os import getenv
+
+storage_type = getenv("HBNB_TYPE_STORAGE")
+
+
+class Amenity(BaseModel, Base):
+    """ documment doc """
+    __tablename__ = 'amenities'
+    if storage_type == 'db':
+        name = Column(String(128), nullable=False)
+        place_amenities = relationship('Place', secondary='place_amenity',
+                                       back_populates="amenities")
+    else:
+        name = ""
